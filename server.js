78525e5f0598c253d12d6eeaa29ee2bc9a1dfdbb
ESrinivasu <30769901+ESrinivasu@users.noreply.github.com>
2017-08-08 00:01:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send("Hai ");
 });
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send("Hello");
 });
 
 app.get('/ui/madi.png', function (req, res) {
